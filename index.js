2305df3b28272d420106665fdcb43ffeb66d1595
Andrew54757 <andrews54757@gmail.com>
2016-09-15 15:01:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var request = require('./minirequest')
-
-module.exports = function() {
+module.exports = function(url) {
+  request(url,function(error,response,body) {
+    if (!error && respone.statusCode = 200) {
+      
+      
+      
+    } else {
+      return "ERROR: Couldnt contact servers."
+      
+    }
+  })
+  
+  
   
   
   
